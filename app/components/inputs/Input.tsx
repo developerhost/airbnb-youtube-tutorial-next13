de3c2bc814cf21e40dc60154ad8d402fbfda7f7d
kojin <kanonhara0609@gmail.com>
2023-05-24 21:54:13 +0900

    start create listing api 4:44:07

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -57,26 +57,26 @@ const Input: React.FC<InputProps> = ({
                 ${errors[id] ? 'forcus:border-rose-500' : 'forcus:border-black'}
             `}
         />
-        <label
+        <label 
             className={`
-                absolute
+                absolute 
                 text-md
-                duration-150
-                transform
-                -translate-y-3
-                top-5
-                z-10
-                origin-[0]
+                duration-150 
+                transform 
+                -translate-y-3 
+                top-5 
+                z-10 
+                origin-[0] 
                 ${formatPrice ? 'left-9' : 'left-4'}
-                peer-placeholder-shown:scale-100
-                peer-placeholder-shown:translate-y-0
-                peer-fucus:scale-75
-                peer-fucus:translate-y-4
-                ${errors[id] ? 'text-rose-500' : 'text-neutral-500'}
+                peer-placeholder-shown:scale-100 
+                peer-placeholder-shown:translate-y-0 
+                peer-focus:scale-75
+                peer-focus:-translate-y-4
+                ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
             `}
         >
             {label}
-        </label>
+      </label>
     </div>
   )
 }
