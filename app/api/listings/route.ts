d1484411bf3db0c299364a448299940cab444653
kojin <kanonhara0609@gmail.com>
2023-05-25 21:08:38 +0900

    create route api create db data 4:48:32

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -2,3 +2,49 @@ import { NextResponse } from "next/server";
 
 import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export async function POST(
+    request: Request,
+) {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) {
+        return NextResponse.error();
+    }
+
+    const body = await request.json();
+    const {
+        title,
+        description,
+        price,
+        location,
+        imageSrc,
+        category,
+        roomCount,
+        bathroomCount,
+        guestCount,
+    } = body;
+
+    Object.keys(body).forEach((value: any) => {
+        if (!body[value]) {
+            NextResponse.error();
+        }
+    });
+
+    const listing = await prisma.listing.create({
+        data: {
+            title,
+            description,
+            price: parseInt(price, 10),
+            locationValue: location.value,
+            imageSrc,
+            category,
+            roomCount,
+            bathroomCount,
+            guestCount,
+            userId: currentUser.id,
+        }
+    });
+
+    return NextResponse.json(listing);
+}
